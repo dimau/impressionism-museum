1a6400fb7669b1cef13b1077679ffd62d9cd94fe
Dmitrii <dimau777@gmail.com>
2023-02-14 23:03:37 +0400

    Promo and Bonuses - first attempt

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,5 @@ import "../styles/normalizer.scss";
 import "../styles/button.scss";
 import "../styles/index.scss";
 import "../styles/header.scss";
+import "../styles/promo.scss";
+import "../styles/bonuses.scss";
