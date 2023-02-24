5bf2b576423de92c4ad7698bcb2ad11b9c51322e
Dmitrii <dimau777@gmail.com>
2023-02-24 18:39:28 +0400

    fix: defer scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+// Styles basic and specific for each section
 import "../styles/normalizer.scss";
 import "../styles/fonts.scss";
 import "../styles/button.scss";
@@ -6,14 +7,16 @@ import "../styles/index.scss";
 import "../styles/header.scss";
 import "../styles/promo.scss";
 import "../styles/bonuses.scss";
-import "./stackingCards";
 import "../styles/tasks.scss";
 import "../styles/steps.scss";
 import "../styles/about.scss";
 import "../styles/accordion.scss";
-import {Accordion} from "./accordion";
 import "../styles/faq.scss";
 import "../styles/footer.scss";
+
+// JS for components
+import "./stackingCards";
+import {Accordion} from "./accordion";
 
 // Initialization Accordion component
 new Accordion(document.querySelector(".accordion"), {
